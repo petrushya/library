a1e3c1ccb71595da36e2ae631fc1af6a2da6b08a
petrushya <108490184+petrushya@users.noreply.github.com>
2024-06-20 23:13:45 +0300

    changes Book constructor to class

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 
-const myLibrary = [
-  new Book('some new book','very famous author',333),
-  new Book('very old book','one of several authors',1111)
-];
 const form = document.querySelector('form');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
@@ -12,6 +8,51 @@ const btnAddBook = document.querySelector('#btnAddBook');
 const btnOpenForm = document.querySelector('.btnOpenForm');
 const btnClozeForm = document.querySelector('.btnClozeForm');
 const btnClean = document.querySelector('.btnReset');
+
+class Book {
+  constructor(title, author, pages){
+    this.bookInfo = [this.title = title, this.author = author, this.pages = pages];
+  }
+  status(){
+    const select = document.createElement('select');
+    const optionOne = document.createElement('option');
+    optionOne.textContent = 'not read yet';
+    optionOne.setAttribute('name', 'not');
+    optionOne.setAttribute('selected', '');
+    select.appendChild(optionOne);
+    const optionTwo = document.createElement('option');
+    optionTwo.textContent = 'alreade read';
+    optionTwo.setAttribute('name', 'yes');
+    select.appendChild(optionTwo);
+    select.onchange = () => {
+      optionOne.setAttribute('disabled', '');
+      select.blur();
+    };
+    return select;
+  }
+  btnDelet(){
+    const button = document.createElement('button');
+    button.textContent = 'delete';
+    button.setAttribute('type', 'button');
+    button.className = 'deleteBook';
+    return button;
+  }
+  bookData(){
+    const arr = [];
+    this.bookInfo.forEach(item => {
+      const p = document.createElement('p');
+      p.textContent = item;
+      arr.push(p);
+    });
+    arr.push(this.status(), this.btnDelet());
+    return arr;
+  }
+}
+
+const myLibrary = [
+  new Book('some new book','very famous author',333).bookData(),
+  new Book('very old book','one of several authors',1111).bookData()
+];
 
 btnOpenForm.onclick = () => {
   form.className = 'transform';
@@ -49,48 +90,8 @@ window.onkeydown = (e) => {
   if(e.key === 'Escape' && form.className === 'transform') form.removeAttribute('class');
 }
 
-function Book(title,author,pages){
-  this.bookInfo = [title, author, pages];
-  this.status = function(){
-    const select = document.createElement('select');
-    select.setAttribute('name', 'choice');
-    const optionOne = document.createElement('option');
-    optionOne.setAttribute('value', 'not');
-    optionOne.setAttribute('selected', '');
-    optionOne.textContent='not read yet';
-    select.appendChild(optionOne);
-    const optionTwo = document.createElement('option');
-    optionTwo.setAttribute('value', 'yes');
-    optionTwo.textContent='already read';
-    select.appendChild(optionTwo);
-    select.onchange = (e) => {
-      if(e.target.value === optionTwo.value) optionOne.setAttribute('disabled', '');
-      select.blur();
-    };
-    return select;
-  };
-  this.btnDelet = function(){
-    const btnDelet = document.createElement('button');
-    btnDelet.textContent = 'delete';
-    btnDelet.className = 'deleteBook';
-    btnDelet.setAttribute('type', 'button');
-    return btnDelet;
-  };
-  this.bookData = function(){
-    const arr = [];
-    this.bookInfo.forEach(item => {
-      const para = document.createElement('p');
-      para.textContent = item;
-      arr.push(para);
-    });
-    arr.push(this.status(), this.btnDelet());
-    return arr;
-  };
-  return this.bookData();
-}
-
 function addBookToLibrary(title,author,pages){
-  myLibrary.push(new Book(title,author,pages));
+  myLibrary.push(new Book(title,author,pages).bookData());
 }
 
 function displayBook(){
