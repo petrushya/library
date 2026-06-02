20eb1b2c50e7a3e1fe974a40615675176c5d8b9e
petrushya <108490184+petrushya@users.noreply.github.com>
2026-06-02 18:38:51 +0300

    restore main.js

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
-const form = document.querySelector('form');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const tbody = document.querySelector('tbody');
-const btnAddBook = document.querySelector('#btnAddBook');
-const btnOpenForm = document.querySelector('.btnOpenForm');
-const btnClozeForm = document.querySelector('.btnClozeForm');
-const btnClean = document.querySelector('.btnReset');
+const form = document.querySelector("form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const tbody = document.querySelector("tbody");
+const btnAddBook = document.querySelector("#btnAddBook");
+const btnOpenForm = document.querySelector(".btnOpenForm");
+const btnClozeForm = document.querySelector(".btnClozeForm");
+const btnClean = document.querySelector(".btnReset");
 
 class Book {
   constructor(title, author, pages) {
-    this.bookInfo =
-      [this.title = title, this.author = author, this.pages = pages];
+    this.bookInfo = [(this.title = title), (this.author = author), (this.pages = pages)];
   }
   #status() {
-    const select = document.createElement('select');
-    const optionOne = document.createElement('option');
-    const optionTwo = document.createElement('option');
-    optionOne.textContent = 'not read yet';
-    optionOne.setAttribute('name', 'not');
-    optionOne.setAttribute('selected', '');
+    const select = document.createElement("select");
+    const optionOne = document.createElement("option");
+    const optionTwo = document.createElement("option");
+    optionOne.textContent = "not read yet";
+    optionOne.setAttribute("name", "not");
+    optionOne.setAttribute("selected", "");
     select.appendChild(optionOne);
-    optionTwo.textContent = 'alreade read';
-    optionTwo.setAttribute('name', 'yes');
+    optionTwo.textContent = "alreade read";
+    optionTwo.setAttribute("name", "yes");
     select.appendChild(optionTwo);
     select.onchange = () => {
-      optionOne.setAttribute('disabled', '');
+      optionOne.setAttribute("disabled", "");
       select.blur();
     };
     return select;
   }
   #btnDelet() {
-    const button = document.createElement('button');
-    button.textContent = 'delete';
-    button.type = 'button';
+    const button = document.createElement("button");
+    button.textContent = "delete";
+    button.type = "button";
     return button;
   }
   bookData() {
     const arr = [];
-    this.bookInfo.forEach(item => {
-      const p = document.createElement('p');
+    this.bookInfo.forEach((item) => {
+      const p = document.createElement("p");
       p.textContent = item;
       arr.push(p);
     });
@@ -49,64 +48,64 @@ class Book {
 }
 
 const myLibrary = [
-  (new Book('some new book', 'very famous author', 333)).bookData(),
-  (new Book('very old book', 'one of several authors', 1111)).bookData(),
+  new Book("some new book", "very famous author", 333).bookData(),
+  new Book("very old book", "one of several authors", 1111).bookData(),
 ];
 
 btnOpenForm.onclick = () => {
-  form.className = 'transform';
+  form.className = "transform";
   btnOpenForm.blur();
   title.focus();
-}
+};
 
 form.onsubmit = (e) => {
   e.preventDefault();
   if (title.value && author.value && pages.value) {
     addBookToLibrary(title.value, author.value, pages.value);
     displayBook();
-    form.removeAttribute('class');
-    title.value = '';
-    author.value = '';
-    pages.value = '';
+    form.removeAttribute("class");
+    title.value = "";
+    author.value = "";
+    pages.value = "";
   } else {
     btnAddBook.blur();
   }
-}
+};
 
 btnClean.onclick = () => {
   title.focus();
   btnClean.blur();
-}
+};
 
 btnClozeForm.onclick = () => {
-  form.removeAttribute('class');
-  title.value = '';
-  author.value = '';
-  pages.value = '';
-}
+  form.removeAttribute("class");
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+};
 
 window.onkeydown = (e) => {
-  if (e.key === 'Escape' && form.className === 'transform') {
-    form.removeAttribute('class');
-    title.value = '';
-    author.value = '';
-    pages.value = '';
+  if (e.key === "Escape" && form.className === "transform") {
+    form.removeAttribute("class");
+    title.value = "";
+    author.value = "";
+    pages.value = "";
   }
-}
+};
 
 function displayBook() {
-  tbody.textContent = '';
+  tbody.textContent = "";
   for (let index = 0; index < myLibrary.length; index++) {
-    const tableRow = document.createElement('tr');
-    const cellHead = document.createElement('th');
-    cellHead.scope = 'row';
+    const tableRow = document.createElement("tr");
+    const cellHead = document.createElement("th");
+    cellHead.scope = "row";
     cellHead.textContent = index + 1;
     tableRow.appendChild(cellHead);
-    myLibrary[index].forEach(item => {
-      const tableCell = document.createElement('td');
+    myLibrary[index].forEach((item) => {
+      const tableCell = document.createElement("td");
       tableCell.appendChild(item);
       tableRow.appendChild(tableCell);
-      if(item.tagName.toLowerCase() === 'button') {
+      if (item.tagName.toLowerCase() === "button") {
         item.onclick = () => {
           myLibrary.splice(index, 1);
           displayBook();
@@ -114,7 +113,7 @@ function displayBook() {
       }
     });
     tbody.appendChild(tableRow);
-  };
+  }
 }
 
 displayBook();
